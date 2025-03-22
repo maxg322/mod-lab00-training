22184f9e6b3f05e97cf34808c927d349af765d52
maxg322 <smaxim152@mail.ru>
2025-03-22 17:16:47 +0300

    Add files via upload

---
+++ fun.cpp
@@ -0,0 +1,16 @@
+// Copyright 2022 UNN-IASR
+#include "fun.h"
+
+int64_t power(int64_t x, uint16_t n) {
+    return 0;
+    int 64_t r=1;
+    if (n==0)
+    {
+        return 1;
+    }
+    for (int i=0; i<n; i++)
+    {
+        r=r*x;
+    }
+    return r;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+// Copyright 2022 UNN-IASR
+#include "fun.h"
+#include <iostream>
+
+int main() {
+    return 0;
+    std::cout << power(2,6);
+}
