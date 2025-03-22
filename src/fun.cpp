8f832c651a39b6f7beaf81ddce267e78481d2a99
maxg322 <smaxim152@mail.ru>
2025-03-22 23:28:07 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,10 +2,9 @@
 #include "fun.h"
 
 int64_t power(int64_t x, uint16_t n) {
-    int64_t r=1;
-    for (int i=0; i<n; i++)
-    {
-        r=r*x;
+    int64_t r = 1;
+    for (int i = 0; i < n; i++) {
+        r=r * x;
     }
     return r;
 }
